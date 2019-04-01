24ac8d21ecbf2bcf37c9c58c9fc895423a85f0b3
Hamish Wadsworth <9835077@student.toiohomai.ac.nz>
2019-04-01 16:42:13 +1300

    qu4

--- Assignment1/Assessment1/Question4/ProgramQ4.cs
+++ Assignment1/Assessment1/Question4/ProgramQ4.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 using Question3;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Question4
 {
@@ -25,6 +27,7 @@ namespace Question4
 
         public static void Menu()
         {
+            string[] people = {"Trady Joe", "Dr Windy Pops", "Grandpa Bob", "Billy the kid", "Chris on crutches" };
             Console.WriteLine("Choose three customers to add to the queue:");
             Console.WriteLine("\n1 - Tradesman Joe\n2 - Dr Windy Pops\n3 - Grandpa Bob\n4 - Billy the kid\n5 - Chris on crutches");
             string input = Console.ReadLine();
@@ -32,27 +35,40 @@ namespace Question4
             int qSize = 3;
             for (int i = 0; i < qSize; i++)
             {
-                switch (input)
-                {
-                    case "1":
-                        MyQueue<ArrayList>.Enqueue(input);
-                        break;
-                    case "2":
-                        MyQueue<ArrayList>.Enqueue(input);
-                        break;
-                    case "3":
-                        MyQueue<ArrayList>.Enqueue(input);
-                        break;
-                    case "4":
-                        MyQueue<ArrayList>.Enqueue(input);
-                        break;
-                    case "5":
-                        MyQueue<ArrayList>.Enqueue(input);
-                        break;
-                    default:
-                        break;
-                }
+                    if (input == "1")
+                    {
+                        MyQueue<ArrayList>.Enqueue(people[0]);
+                        //WaitTime(1);
+                        MyQueue<ArrayList>.DeQueue();
+                    }
+                    else if (input == "2")
+                    {
+                        MyQueue<ArrayList>.Enqueue(people[1]);
+                    }
+                    else if (input == "3")
+                    {
+                        MyQueue<ArrayList>.Enqueue(people[2]);
+                    }
+                    else if (input == "4")
+                    {
+                        MyQueue<ArrayList>.Enqueue(people[3]);
+                    }
+                    else if (input == "5")
+                    {
+                        MyQueue<ArrayList>.Enqueue(people[4]);
+                    }
             }
+        }
+
+        public static void WaitTime(int seconds)
+        {
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine("[DONE]");
+
+            //Stopwatch stopwatch = Stopwatch.StartNew();
+            //Thread.Sleep(1000);
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
     }
 
@@ -60,7 +76,7 @@ namespace Question4
     {
         public static void AddedToQueue(string userName)
         {
-            Console.WriteLine("{0} has been added to the queue.", userName);
+            Console.WriteLine("{0} has been added to the queue.", people[]);
         }
     }
 
